177fa88790207558020e8a75eda91f3b6b17ae14
Alina Radyukevich <lina.radyukevich98@gmail.com>
2025-10-29 05:46:11 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/betting-software/',
   plugins: [react(), svgr(
     {
       svgrOptions: {
